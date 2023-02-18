513db989c9ed2ba80de15d47b9cf1d5a294717ea
Amine Mahjoub <31483078+eminemahjoub@users.noreply.github.com>
2023-02-18 18:54:30 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function mostrarOcultarMenu(){
 }
 
 function seleccionar(){
-    //oculto el menu una vez que selecciono una opcion
     document.getElementById("nav").classList = "";
     menuVisible = false;
 }
